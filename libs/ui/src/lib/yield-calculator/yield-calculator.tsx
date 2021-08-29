fc8e668097baf91942a3fb57ed1e6e07585df5a3
Mauricio Robayo <rfmajo@gmail.com>
2021-08-29 10:07:11 -0500

    chore: simplify calculations
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- libs/ui/src/lib/yield-calculator/yield-calculator.tsx
+++ libs/ui/src/lib/yield-calculator/yield-calculator.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { ChangeEvent } from 'react';
 import { useState } from 'react';
-import YieldTable from '../yield-table/yield-table';
+// import YieldTable from '../yield-table/yield-table';
 import {
   getYieldFactor,
   getWeightBasedOnYieldFactor,
@@ -14,20 +14,15 @@ export interface YieldCalculatorProps {}
 const StyledYieldCalculator = styled.div``;
 
 export function YieldCalculator(props: YieldCalculatorProps) {
-  const maxSampleSize = 500;
-  const minSampleSize = 100;
   const baseYieldFactor = 94;
   const baseRefPrice = 1500000;
   const baseWeight = getWeightBasedOnYieldFactor(baseYieldFactor);
 
   const [refPrice, setRefPrice] = useState<number>(baseRefPrice);
   const [sampleSize, setSampleSize] = useState(250);
-  // TODO: this should be number and should include all decimal places
-  const [premiumGrams, setPremiumGrams] = useState(baseWeight.toFixed(2));
-  const [sellPrice, setSellPrice] = useState<number | null>(null);
-  const [yieldFactor, setYieldFactor] = useState<number | null>(
-    baseYieldFactor
-  );
+  const [premiumGrams, setPremiumGrams] = useState(baseWeight);
+  const [sellPrice, setSellPrice] = useState<number>(baseRefPrice);
+  const [yieldFactor, setYieldFactor] = useState<number>(baseYieldFactor);
 
   const onRefPriceChange = (e: ChangeEvent<HTMLInputElement>) => {
     const refPrice = Number(e.target.value);
@@ -38,43 +33,22 @@ export function YieldCalculator(props: YieldCalculatorProps) {
     }
 
     setRefPrice(refPrice);
-
-    const sellPrice = getSellPrice(baseYieldFactor, yieldFactor, refPrice);
-    setSellPrice(sellPrice);
+    setSellPrice(getSellPrice(baseYieldFactor, yieldFactor, refPrice));
   };
 
   const onSampleSizeChange = (e: ChangeEvent<HTMLInputElement>) => {
     const sampleSize = Number(e.target.value);
 
-    if (sampleSize > maxSampleSize || sampleSize < minSampleSize) {
-      setYieldFactor(null);
-      setSampleSize(sampleSize);
-      return;
-    }
-
-    const yieldFactor = getYieldFactor(Number(premiumGrams), sampleSize);
-
-    setYieldFactor(yieldFactor);
+    setYieldFactor(getYieldFactor(Number(premiumGrams), sampleSize));
     setSampleSize(sampleSize);
   };
 
   const onGramsChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const gramsString = e.target.value;
+    const grams = Number(e.target.value);
 
-    console.log({ gramsString });
-
-    if (!/^\d*\.?\d{0,2}$/.test(gramsString)) {
-      return;
-    }
-
-    const grams = Number(gramsString);
-
-    const yieldFactor = getYieldFactor(grams, sampleSize);
-    const sellPrice = getSellPrice(baseYieldFactor, yieldFactor, refPrice);
-
-    setSellPrice(sellPrice);
-    setYieldFactor(yieldFactor);
-    setPremiumGrams(gramsString);
+    setSellPrice(getSellPrice(baseYieldFactor, yieldFactor, refPrice));
+    setYieldFactor(getYieldFactor(grams, sampleSize));
+    setPremiumGrams(grams);
   };
 
   return (
@@ -89,7 +63,7 @@ export function YieldCalculator(props: YieldCalculatorProps) {
         />
       </div>
       <div>
-        <label htmlFor="sampleSize">Tamaño de la muestra</label>
+        <label htmlFor="sampleSize">Tamaño de la muestra en gramos</label>
         <input
           id="sampleSize"
           onChange={onSampleSizeChange}
@@ -100,7 +74,7 @@ export function YieldCalculator(props: YieldCalculatorProps) {
         />
       </div>
       <div>
-        <label htmlFor="grams">Peso cafe excelso</label>
+        <label htmlFor="grams">Gramos de cafe excelso resultante</label>
         <input
           id="grams"
           onChange={onGramsChange}
@@ -110,19 +84,16 @@ export function YieldCalculator(props: YieldCalculatorProps) {
         />
         <span>gramos.</span>
       </div>
-      <div>
-        Factor de rendimiento:{' '}
-        {typeof yieldFactor === 'number' ? yieldFactor.toFixed(2) : ''}
-      </div>
+      <div>Factor de rendimiento: {yieldFactor}</div>
       <div>Precio de compra: {sellPrice?.toFixed()}</div>
-      <YieldTable
+      {/* <YieldTable
         min={89}
         max={99}
         baseYieldFactor={baseYieldFactor}
         yieldFactor={yieldFactor}
         sampleSize={sampleSize}
         refPrice={refPrice}
-      />
+      /> */}
     </StyledYieldCalculator>
   );
 }
