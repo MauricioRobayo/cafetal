b96f87d56f912c376373a91be5557312d121a6ed
Mauricio Robayo <rfmajo@gmail.com>
2021-08-30 09:37:29 -0500

    refactor: reorganize theme colors
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- apps/web/styles/theme.ts
+++ apps/web/styles/theme.ts
@@ -12,9 +12,9 @@ export const defaultTheme: DefaultTheme = {
     brand: brandColor.string(),
     text1: brandColor.lightness(10).string(),
     text2: brandColor.saturationl(30).lightness(30).string(),
-    surface1: brandColor.saturationl(25).lightness(90).string(),
-    surface2: brandColor.saturationl(20).lightness(99).string(),
-    surface3: brandColor.saturationl(20).lightness(92).string(),
+    surface1: brandColor.saturationl(20).lightness(99).string(),
+    surface2: brandColor.saturationl(20).lightness(92).string(),
+    surface3: brandColor.saturationl(25).lightness(90).string(),
     surface4: brandColor.saturationl(20).lightness(85).string(),
   },
 };
