7cf0701f728cca17f36882c52d0c13e882cacc92
Mauricio Robayo <rfmajo@gmail.com>
2021-08-31 11:36:13 -0500

    chore(number-input): use real and string value inputs
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- libs/ui/src/lib/number-input/number-input.tsx
+++ libs/ui/src/lib/number-input/number-input.tsx
@@ -1,24 +1,16 @@
-import { createComponentStoriesFile } from '@nrwl/react/src/generators/component-story/component-story';
 import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 function formatCurrency(number: number): string {
   const formatter = new Intl.NumberFormat('en-US', {
-    style: 'decimal',
+    style: 'currency',
+    currencyDisplay: 'narrowSymbol',
+    currency: 'COP',
     maximumFractionDigits: 0,
     minimumFractionDigits: 0,
   });
 
   return formatter.format(number);
-}
-
-function parseCurrency(number: string): number | null {
-  const value = Number(number.replace(/,/g, ''));
-  if (Number.isNaN(value)) {
-    return null;
-  }
-
-  return value;
 }
 
 type Modify<T, R> = Omit<T, keyof R> & R;
@@ -49,55 +41,41 @@ export function NumberInput({
   value,
   ...props
 }: NumberInputProps) {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const [realValue, setRealValue] = useState(value);
   const [stringValue, setStringValue] = useState(formatCurrency(value));
-  const [cursorPosition, setCursorPosition] = useState([0, 0]);
+  const [isEditing, setIsEditing] = useState(false);
 
-  useEffect(() => {
-    const [cursorPositionStart, cursorPositionEnd] = cursorPosition;
+  function changeHandler(e: ChangeEvent<HTMLInputElement>) {
+    const value = Number(e.target.value);
+    setRealValue(value);
+    setStringValue(formatCurrency(value));
+    onChange(realValue, stringValue);
+  }
 
-    const offset = [...stringValue]
-      .slice(0, cursorPositionEnd)
-      .filter((char) => char === ',').length;
+  function onStringValueFocus() {
+    setIsEditing(true);
+  }
 
-    inputRef.current?.setSelectionRange(
-      cursorPositionStart + offset,
-      cursorPositionEnd + offset
+  if (isEditing) {
+    return (
+      <StyledNumberInput
+        className={className}
+        value={realValue}
+        onChange={changeHandler}
+        onBlur={() => setIsEditing(false)}
+        type="text"
+        inputMode="decimal"
+      />
     );
-  }, [stringValue, cursorPosition]);
-
-  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    const rawValue = e.target.value;
-    const cursorPositionStart = Number(inputRef.current?.selectionStart);
-    const cursorPositionEnd = Number(inputRef.current?.selectionEnd);
-    const offset = [...rawValue]
-      .slice(0, cursorPositionEnd)
-      .filter((char) => char === ',').length;
-
-    const value = parseCurrency(rawValue);
-
-    if (value === null) {
-      return;
-    }
-
-    setCursorPosition([
-      cursorPositionStart - offset,
-      cursorPositionEnd - offset,
-    ]);
-
-    const stringValue = formatCurrency(value);
-    setStringValue(stringValue);
-    onChange(value, stringValue);
-  };
+  }
 
   return (
     <StyledNumberInput
       className={className}
       value={stringValue}
-      onChange={changeHandler}
       type="text"
-      inputMode="decimal"
-      ref={inputRef}
+      onFocus={onStringValueFocus}
+      readOnly
       {...props}
     />
   );
