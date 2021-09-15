6600548b50338599b2145a839f17605baa742116
Mauricio Robayo <rfmajo@gmail.com>
2021-09-15 15:49:39 -0500

    feat: add scripts lib
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

---
+++ libs/scripts/src/process-ref-price/index.ts
@@ -0,0 +1,58 @@
+import { FileDownloader, uploadFile } from '@calculadora-cafetera/utils';
+import { RefPriceRepository } from '@calculadora-cafetera/repositories';
+import { parser } from '@calculadora-cafetera/utils';
+
+const repo = new RefPriceRepository();
+
+async function processRefPrice(): Promise<void> {
+  try {
+    const latestRefPrice = await repo.getLatest();
+    const fd = new FileDownloader(latestRefPrice?.etag);
+    const downloadResult = await fd.downloadFileWithExponentialBackOff();
+    const { retries, elapseTimeMs, status } = downloadResult;
+
+    console.log('getRefPrice: downloadResult', {
+      elapseTimeMs,
+      retries,
+      status,
+    });
+
+    if (FileDownloader.isSuccessfulExponentialBackOffResult(downloadResult)) {
+      const { data, etag, lastModified } = downloadResult;
+      let fileName = '';
+
+      try {
+        const { pdfInfo, content, refPrice } = await parser(data);
+        fileName = `${refPrice.date.slice(0, 10)}.pdf`;
+
+        await repo.save({
+          content,
+          createdAt: Date.now(),
+          etag,
+          fileName,
+          lastModified,
+          pdfInfo,
+          refPrice,
+        });
+      } catch (err) {
+        console.log('getRefPrice: parsing pdf file failed', err);
+      }
+
+      await uploadFile(data, fileName);
+
+      console.log('getRefPrice: Process completed', {
+        etag,
+        fileName,
+        lastModified,
+      });
+
+      process.exit();
+    }
+  } catch (err) {
+    console.log('getRefPrice: failed', err);
+  }
+
+  process.exit(1);
+}
+
+processRefPrice();
--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,4 +1,5 @@
 export * from './get-env-vars';
 export * from './ref-price-calculator';
 export * from './ref-price-downloader';
+export * from './ref-price-parser';
 export * from './uploader';
