71be73da0c3b3c1f126a2a4b3e07a8a841ebae4e
Mauricio Robayo <rfmajo@gmail.com>
2021-09-16 05:32:26 -0500

    chore: rename ref-price app
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- apps/ref-price/jest.config.js
+++ apps/ref-price/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'reference-price',
+  displayName: 'ref-price',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/reference-price',
+  coverageDirectory: '../../coverage/apps/ref-price',
 };
