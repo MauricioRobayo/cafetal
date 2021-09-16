716350f7723e3da4ec8e361df3cc444a0fca06df
Mauricio Robayo <rfmajo@gmail.com>
2021-09-16 10:38:23 -0500

    refactor: use functional approach instead of class
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- apps/ref-price/src/app/downloader/downloader.ts
+++ apps/ref-price/src/app/downloader/downloader.ts
@@ -12,7 +12,6 @@ interface DownloadResult {
 interface ExponentialBackOffResult {
   elapseTimeMs: number;
   maxExecutionExceeded: boolean;
-  maxExecutionTime: number;
   retries: number;
   status: 'success' | 'failed';
 }
@@ -20,92 +19,101 @@ interface ExponentialBackOffResult {
 type SuccessfulExponentialBackOffResult = DownloadResult &
   ExponentialBackOffResult;
 
-export class FileDownloader {
-  constructor(private etag?: string) {}
+async function fetchFile(etag?: string): Promise<DownloadResult | null> {
+  const headers: { [k: string]: string } = {};
 
-  async downloadFile(): Promise<DownloadResult | null> {
-    const headers: { [k: string]: string } = {};
+  if (etag) {
+    headers['If-None-Match'] = etag;
+  }
 
-    if (this.etag) {
-      headers['If-None-Match'] = this.etag;
+  try {
+    const {
+      data,
+      headers: { etag, 'last-modified': lastModified },
+    } = await axios.get<ArrayBuffer>(fileUrl, {
+      responseType: 'arraybuffer',
+      headers,
+    });
+
+    return {
+      data: Buffer.from(data),
+      etag,
+      lastModified,
+    };
+  } catch (err) {
+    if (axios.isAxiosError(err) && err.response?.status === 304) {
+      return null;
     }
 
-    try {
-      const {
-        data,
-        headers: { etag, 'last-modified': lastModified },
-      } = await axios.get<ArrayBuffer>(fileUrl, {
-        responseType: 'arraybuffer',
-        headers,
-      });
+    throw new Error(`downloadFile: get request failed ${err}`);
+  }
+}
 
+interface DownloadFileWithExponentialBackOffOptons {
+  etag?: string;
+  maxExecutionTimeMs: number;
+  minRndDeltaMs: number;
+  maxRndDeltaMs: number;
+  delayFactor: number;
+  fetcher?: typeof fetchFile;
+}
+export async function downloadFileWithExponentialBackOff({
+  etag,
+  maxExecutionTimeMs,
+  minRndDeltaMs,
+  maxRndDeltaMs,
+  delayFactor,
+  fetcher = fetchFile,
+}: DownloadFileWithExponentialBackOffOptons): Promise<
+  SuccessfulExponentialBackOffResult | ExponentialBackOffResult
+> {
+  let delayMs = 0;
+  let retries = 0;
+  const startTime = Date.now();
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const downloadResult = await fetcher(etag);
+    const elapseTimeMs = Date.now() - startTime;
+
+    if (downloadResult) {
       return {
-        data: Buffer.from(data),
-        etag,
-        lastModified,
-      };
-    } catch (err) {
-      if (axios.isAxiosError(err) && err.response?.status === 304) {
-        return null;
-      }
-
-      throw new Error(`downloadFile: get request failed ${err}`);
-    }
-  }
-
-  async downloadFileWithExponentialBackOff(
-    maxExecutionTime = 8 * 60 * 60 * 1000
-  ): Promise<SuccessfulExponentialBackOffResult | ExponentialBackOffResult> {
-    let delayMs = 0;
-    let retries = 0;
-    const startTime = Date.now();
-
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      const downloadResult = await this.downloadFile();
-      const elapseTimeMs = Date.now() - startTime;
-
-      if (downloadResult) {
-        return {
-          ...downloadResult,
-          elapseTimeMs,
-          maxExecutionExceeded: false,
-          maxExecutionTime,
-          retries,
-          status: 'success',
-        };
-      }
-
-      if (elapseTimeMs >= maxExecutionTime) {
-        return {
-          elapseTimeMs,
-          maxExecutionExceeded: true,
-          maxExecutionTime,
-          retries,
-          status: 'failed',
-        };
-      }
-
-      retries = retries + 1;
-
-      const randomDeltaMs = randBetween(60 * 1000, 4 * 60 * 1000);
-      delayMs = (delayMs + randomDeltaMs) * 2;
-
-      console.log('downloadFileWithExponentialBackOff', {
-        delayMs,
+        ...downloadResult,
         elapseTimeMs,
+        maxExecutionExceeded: false,
         retries,
-      });
-
-      await new Promise((resolve) => setTimeout(resolve, delayMs));
+        status: 'success',
+      };
     }
-  }
 
-  static isSuccessfulExponentialBackOffResult(
-    result: SuccessfulExponentialBackOffResult | ExponentialBackOffResult
-  ): result is SuccessfulExponentialBackOffResult {
-    return result.status === 'success';
+    if (elapseTimeMs >= maxExecutionTimeMs) {
+      return {
+        elapseTimeMs,
+        maxExecutionExceeded: true,
+        retries,
+        status: 'failed',
+      };
+    }
+
+    retries = retries + 1;
+
+    const randomDeltaMs = randBetween(minRndDeltaMs, maxRndDeltaMs);
+    delayMs = (delayMs + randomDeltaMs) * delayFactor;
+
+    console.log('downloadFileWithExponentialBackOff', {
+      delayMs,
+      elapseTimeMs,
+      retries,
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, delayMs));
   }
+}
+
+export function isSuccessfulExponentialBackOffResult(
+  result: SuccessfulExponentialBackOffResult | ExponentialBackOffResult
+): result is SuccessfulExponentialBackOffResult {
+  return result.status === 'success';
 }
 
 function randBetween(min: number, max: number): number {
--- apps/ref-price/src/main.ts
+++ apps/ref-price/src/main.ts
@@ -1,4 +1,7 @@
-import { FileDownloader } from './app/downloader';
+import {
+  downloadFileWithExponentialBackOff,
+  isSuccessfulExponentialBackOffResult,
+} from './app/downloader';
 import { parser } from './app/parser';
 import { uploadFile } from './app/uploader';
 import { RefPriceRepository } from '@cafetal/repositories';
@@ -8,8 +11,13 @@ const repo = new RefPriceRepository();
 async function processRefPrice(): Promise<void> {
   try {
     const latestRefPrice = await repo.getLatest();
-    const fd = new FileDownloader(latestRefPrice?.etag);
-    const downloadResult = await fd.downloadFileWithExponentialBackOff();
+    const downloadResult = await downloadFileWithExponentialBackOff({
+      etag: latestRefPrice?.etag,
+      maxExecutionTimeMs: 4 * 1000 * 60 * 60,
+      minRndDeltaMs: 0.5 * 1000 * 60,
+      maxRndDeltaMs: 1.5 * 1000 * 60,
+      delayFactor: 1.1,
+    });
     const { retries, elapseTimeMs, status } = downloadResult;
 
     console.log('getRefPrice: downloadResult', {
@@ -18,7 +26,7 @@ async function processRefPrice(): Promise<void> {
       status,
     });
 
-    if (FileDownloader.isSuccessfulExponentialBackOffResult(downloadResult)) {
+    if (isSuccessfulExponentialBackOffResult(downloadResult)) {
       const { data, etag, lastModified } = downloadResult;
       let fileName = '';
 
