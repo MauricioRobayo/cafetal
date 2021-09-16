f6e3c3e38bbe30aba470f7adf64715c34ba1fbc4
Mauricio Robayo <rfmajo@gmail.com>
2021-09-16 13:49:35 -0500

    test: downloader tests
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- apps/ref-price/src/app/downloader/downloader.spec.ts
+++ apps/ref-price/src/app/downloader/downloader.spec.ts
@@ -23,7 +23,7 @@ describe('maxExecutionTime exceeded', () => {
     },
   ];
   it.each(cases)(
-    'should exponentially try to download delay factor of $delay',
+    'should stop after maxExecutionTime exceeded with a delay factor of $delayFactor',
     async ({ delayFactor, expectedDelays }) => {
       const logSpy = jest
         .spyOn(console, 'log')
@@ -48,9 +48,69 @@ describe('maxExecutionTime exceeded', () => {
         ])
       );
       expect(data.elapseTimeMs).toBeGreaterThanOrEqual(maxExecutionTimeMs);
-      expect(data.maxExecutionExceeded).toBe(true);
       expect(data.retries).toBe(expectedDelays.length - 1);
-      expect(data.status).toBe('failed');
+      expect(data.status).toBe('maxExecutionTime exceeded');
     }
   );
+});
+
+describe('maxRetries exceeded', () => {
+  const minRndDeltaMs = 25;
+  const maxRndDeltaMs = 25;
+  const cases = [
+    {
+      maxRetries: 0,
+      expectedDelays: [0],
+    },
+    {
+      maxRetries: 3,
+      expectedDelays: [0, 50, 150, 350],
+    },
+  ];
+
+  it.each(cases)(
+    'should exponentially try to download with $maxRetries retries',
+    async ({ maxRetries, expectedDelays }) => {
+      const logSpy = jest
+        .spyOn(console, 'log')
+        .mockImplementation(() => void null);
+      const data = await downloadFileWithExponentialBackOff({
+        minRndDeltaMs,
+        maxRndDeltaMs,
+        maxRetries,
+        fetcher: () => new Promise((resolve) => resolve(null)),
+        sleep: () => new Promise((resolve) => resolve()),
+      });
+
+      expect(logSpy).toBeCalledTimes(expectedDelays.length);
+      expect(logSpy.mock.calls).toEqual(
+        expectedDelays.map((expectedDelay, index) => [
+          'downloadFileWithExponentialBackOff',
+          {
+            delayMs: expectedDelay,
+            elapseTimeMs: expect.any(Number),
+            retries: index,
+          },
+        ])
+      );
+      expect(data.retries).toBe(expectedDelays.length - 1);
+      expect(data.status).toBe('maxRetries exceeded');
+    }
+  );
+
+  it('should stop after default max retries exceeded', async () => {
+    const logSpy = jest
+      .spyOn(console, 'log')
+      .mockImplementation(() => void null);
+    const data = await downloadFileWithExponentialBackOff({
+      minRndDeltaMs,
+      maxRndDeltaMs,
+      fetcher: () => new Promise((resolve) => resolve(null)),
+      sleep: () => new Promise((resolve) => resolve()),
+    });
+
+    expect(data.retries).toBe(25);
+    expect(data.status).toBe('maxRetries exceeded');
+    expect(logSpy).toBeCalledTimes(26);
+  });
 });
